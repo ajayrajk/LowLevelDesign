1dbfeac44efdf400953323569f9fd9b3bfc4dfa2
Ajay RajPut <ajayhbti11@gmail.com>
2026-05-19 01:36:15 +0530

    update
    singleton and factory pattern update

--- src/main/java/org/designpattern/creational/factory/withFactory/Main.java
+++ src/main/java/org/designpattern/creational/factory/withFactory/Main.java
@@ -4,9 +4,11 @@ import org.designpattern.creational.factory.Database;
 
 public class Main {
     public static void main(String[] args) {
+        // Open close still need to follow.
         Database db1=DatabaseFactory.getDataBaseFactory("msql");
         Database db2=DatabaseFactory.getDataBaseFactory("psql");
 
+        // More optimized version with follow rhe open close principle as well.
         Database db3=DatabaseFactoryOptimize.getDatabaseInstance("msql");
         Database db4=DatabaseFactoryOptimize.getDatabaseInstance("psql");
     }
--- src/main/java/org/designpattern/creational/singleton/withSingleton/Logger.java
+++ src/main/java/org/designpattern/creational/singleton/withSingleton/Logger.java
@@ -1,16 +1,21 @@
 package org.designpattern.creational.singleton.withSingleton;
 
 public class Logger {
-     private static Logger  logger;
+     //volatile is use for visibility
+     private static volatile Logger  logger;
 
      // Create a private constructor because from outside the class no once can create that object
      private Logger(){
      }
 
+     // Double lock checking
      public static Logger getInstance(){
          if (logger==null)
-             logger=new Logger();
-
+             synchronized (Logger.class){
+                if (logger==null){
+                    logger=new Logger();
+                }
+             }
          return logger;
      }
 }
